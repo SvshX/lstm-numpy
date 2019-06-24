daf08614e678c10a575072e6cc8fa3a31213e9e0
sash83 <saschamelcher@gmail.com>
2019-06-24 21:29:20 +0200

    initial commit

---
+++ lstm.py
@@ -0,0 +1,7 @@
+import numpy as np 
+from scipy.special import expit as sigmoid
+
+def forget_gate(x, h, Weights_hf, Bias_hf, Weights_xf, Bias_xf, prev_cell_state):
+    forget_hidden  = np.dot(Weights_hf, h) + Bias_hf
+    forget_eventx  = np.dot(Weights_xf, x) + Bias_xf
+    return np.multiply( sigmoid(forget_hidden + forget_eventx), prev_cell_state )
